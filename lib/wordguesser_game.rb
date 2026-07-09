ca71ff7d23ed94d4061ec32d3d94db7e9de433fd
Parsa Faraji Alamouti <parsafaraji@berkeley.edu>
2026-07-09 06:12:49 +0000

    Complete Wordguesser game logic

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,17 +1,58 @@
 class WordGuesserGame
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
+  attr_accessor :word, :guesses, :wrong_guesses
 
-  # Get a word from remote "random word" service
-
+  # Initialize a new game with a word
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
+  # Process one guessed letter
+  def guess(letter)
+    raise ArgumentError if letter.nil?
+    raise ArgumentError if letter.empty?
+    raise ArgumentError unless letter =~ /^[A-Za-z]$/
+
+    letter = letter.downcase
+
+    # Repeated guesses are invalid but should not crash
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    end
+
+    if @word.include?(letter)
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+
+    true
+  end
+
+  # Return the word with unguessed letters replaced by -
+  def word_with_guesses
+    @word.chars.map do |letter|
+      if @guesses.include?(letter)
+        letter
+      else
+        '-'
+      end
+    end.join
+  end
+
+  # Return :win, :lose, or :play
+  def check_win_or_lose
+    if word_with_guesses == @word
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
+  end
+
+  # Get a word from remote "random word" service
   def self.get_random_word
     require 'uri'
     require 'net/http'
